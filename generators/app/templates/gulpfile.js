3fceadac2e5ba53e7024e662f30d4e09d8377666
rwt <rwt@rwtdeMacBook-Pro-2.local>
2016-05-06 14:13:22 +0800

    :smile:

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -11,8 +11,48 @@ const request = require('request');
 const objectAssign = require('object-assign');
 const wpConfig = require('./webpack.config.js');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const execSync = require('child_process').execSync;
+const fs = require('fs');
+const path = require('path');
 
-gulp.task('clean', cb => del(['dist'], cb));
+gulp.task('clean', ['checkVersion'], (cb) => {
+  del(['dist']).then(() => {
+    cb();
+  });
+});
+
+gulp.task('checkVersion', (cb) => {
+  let cmdRst = '';
+  let cwd = process.cwd();
+  try {
+    cmdRst = execSync('git tag', {
+      cwd: cwd,
+      stdio: 'pipe'
+    }).toString();
+  } catch(e) {
+    if (e.message.indexOf('Not a git repository') !== -1) {
+      cb(`No git repository was found in ${cwd}`);
+    }
+  }
+  let pkg;
+  try {
+    pkg = fs.readFileSync(path.join(cwd, 'package.json'), 'utf-8');
+  } catch(e) {
+    if (e.message.indexOf('ENOENT') !== -1)
+     cb(`Can not find package.json in ${cwd}`);
+  }
+  try {
+    pkg = JSON.parse(pkg);
+  } catch (e) {
+    cb(`Can not parse package.json in ${cwd}`);
+  }
+  let curVersion = pkg.version;
+  if (cmdRst.split('\n').filter(v => !!v).indexOf('publish/' + curVersion) === -1) {
+    cb()
+  } else {
+    cb('当前工程版本号已经被打成tag')
+  }
+});
 
 gulp.task('build', ['clean'], () => {
   let config = objectAssign({}, wpConfig, {
