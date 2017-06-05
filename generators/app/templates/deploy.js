4506b751ef7b02bf164421c28c3dc53b45481ff0
rwt <rwt@rwtdeMacBook-Pro-2.local>
2017-06-05 16:48:59 +0800

    :smile:

--- generators/app/templates/deploy.js
+++ generators/app/templates/deploy.js
@@ -1,19 +1,27 @@
 const request = require('request');
+// npm run deploy --dev --suffix=wealth(如果是蜂投需要增加suffix参数)
+let suffix = process.env.npm_config_suffix;
 
 // npm run deploy --dev
 const dev = process.env.npm_config_dev;
 // npm run deploy --test
 const test = process.env.npm_config_test;
-
+console.log(`发布工程名称： aDemo${suffix?`-${suffix}`: ''}${test? '+test' : ''}`)
+if (suffix) {
+  suffix = encodeURIComponent(`-${suffix}`);
+} else {
+  suffix = '';
+}
+let jobName = `<%= name %>${suffix}`;
 if (dev) {
-  request('http://192.168.4.133:8080/buildByToken/build?job=<%= name %>&token=dev',
+  request(`http://192.168.4.133:8080/buildByToken/build?job=${jobName}&token=dev`,
     (error, response, body) => {
       console.log('error:', error);
       console.log('statusCode:', response && response.statusCode);
       console.log('body:', body);
     });
 } else if (test) {
-  request('http://192.168.4.133:8080/buildByToken/build?job=<%= name %>%2Btest&token=test',
+  request(`http://192.168.4.133:8080/buildByToken/build?job=${jobName}%2Btest&token=test`,
     (error, response, body) => {
       console.log('error:', error);
       console.log('statusCode:', response && response.statusCode);
